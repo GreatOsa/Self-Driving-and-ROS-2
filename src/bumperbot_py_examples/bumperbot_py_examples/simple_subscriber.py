528c3e2a848c0657684153df230cd30ab04ef26c
Agwaze Great <agwazegreat@gmail.com>
2026-06-12 12:54:15 +0100

    first commit

---
+++ src/bumperbot_py_examples/bumperbot_py_examples/simple_subscriber.py
@@ -0,0 +1,22 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class Simple_Subscriber(Node):
+    def __init__(self):
+        super().__init__('simple_subscriber')
+        self.sub_=self.create_subscription(String, 'chatter',self.msg_callback,10)
+
+    def msg_callback(self,msg):
+        self.get_logger().info('I heard: %s' % msg.data)
+
+
+def main():
+    rclpy.init()
+    simple_subscriber=Simple_Subscriber()
+    rclpy.spin(simple_subscriber)
+    simple_subscriber.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
